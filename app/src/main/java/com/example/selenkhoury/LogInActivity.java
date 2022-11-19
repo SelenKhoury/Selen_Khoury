18c4ec27cc99f222d83ea17c5c3a0e270617d29b
SelenKhoury <selenkhoury535@gmail.com>
2022-11-19 15:01:47 +0200

    aa

--- app/src/main/java/com/example/selenkhoury/LogInActivity.java
+++ app/src/main/java/com/example/selenkhoury/LogInActivity.java
@@ -1,9 +1,13 @@
 package com.example.selenkhoury;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -25,15 +29,16 @@ public class LogInActivity extends AppCompatActivity {
 
     }
 
+    public void login (View view){
+        String input_User = editTextTextPersonName_login.getText().toString();
+      //  String input_Userpass= editTextNumberPassword_login.getText().toString();
+        if (input_User.length()>0) {
+            Toast.makeText(this, "user logged in !", Toast.LENGTH_LONG).show();
+            Intent intent_main = new Intent(this, MainActivity.class);
+            startActivity(intent_main);
+        }else{
+            Toast.makeText(this,"Empty values,please insert !",Toast.LENGTH_LONG).show();
+        }
 
-    public void Login(View view) {
-        /*if (editTextEmailAddress.getText().toString().equals(""))
-            Toast.makeText(this,"Empty Email",Toast.LENGTH_LONG).show();
-        else if (editTextPassword.getText().toString().equals(""))
-            Toast.makeText(this,"Empty Password",Toast.LENGTH_LONG).show();
-        else if (editTextPersonName.getText().toString().equals(""))
-            Toast.makeText(this,"Empty User Name",Toast.LENGTH_LONG).show();
-        else {
-            Intent i = new Intent(this, HomeActivity.class);
-            startActivity(i);
+    }
 }
